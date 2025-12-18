d2c0f7dbd1db83aad70d4d5fff13f70f479efee9
Chris <33991025+moimeme81@users.noreply.github.com>
2025-12-18 18:02:56 -0500

    Implement async setup entry for HydroEvenements

---
+++ __init__.py
@@ -0,0 +1,16 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+from .coordinator import HydroEvenementsCoordinator
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    coordinator = HydroEvenementsCoordinator(hass)
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    return True
