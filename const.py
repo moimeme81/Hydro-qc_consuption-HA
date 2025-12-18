efc95435e5ac2f4802ef4891f9c79a69fbfc2113
Chris <33991025+moimeme81@users.noreply.github.com>
2025-12-18 18:00:49 -0500

    Add constants for Hydro Quebec API configuration

---
+++ const.py
@@ -0,0 +1,9 @@
+DOMAIN = "hydro_quebec_evenements"
+
+API_URL = (
+    "https://donnees.hydroquebec.com/api/explore/v2.1/"
+    "catalog/datasets/consommation-clients-evenements-pointe/records"
+)
+
+DEFAULT_LIMIT = 24
+DEFAULT_SCAN_INTERVAL = 3600  # seconds
