0354798e6e2d73efc07389350f7a4c6ca71f3d9e
Chris <33991025+moimeme81@users.noreply.github.com>
2025-12-18 18:09:22 -0500

    Replace HydroEvenementsCoordinator with HydroQuebecCoordinator

--- __init__.py
+++ __init__.py
@@ -2,11 +2,11 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
-from .coordinator import HydroEvenementsCoordinator
+from .coordinator import HydroQuebecCoordinator
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    coordinator = HydroEvenementsCoordinator(hass)
+    coordinator = HydroQuebecCoordinator(hass)
     await coordinator.async_config_entry_first_refresh()
 
     hass.data.setdefault(DOMAIN, {})
