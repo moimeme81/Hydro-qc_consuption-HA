bc922215027da157396a3e6a63e18a0cd0b12a8b
Chris <33991025+moimeme81@users.noreply.github.com>
2025-12-18 18:09:51 -0500

    Rename class and update entry title for Hydro-Québec

--- config_flow.py
+++ config_flow.py
@@ -2,11 +2,14 @@ from homeassistant import config_entries
 from .const import DOMAIN
 
 
-class HydroEvenementsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class HydroQuebecConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
-            return self.async_create_entry(title="Hydro-Québec événements", data={})
+            return self.async_create_entry(
+                title="Hydro-Québec – Consommation d’énergie",
+                data={},
+            )
 
         return self.async_show_form(step_id="user")
