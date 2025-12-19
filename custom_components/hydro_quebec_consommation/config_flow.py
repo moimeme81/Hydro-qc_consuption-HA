c691f1083c9616b39b3b9e2f00d9474829a3d703
Chris <33991025+moimeme81@users.noreply.github.com>
2025-12-19 00:04:42 -0500

    Refactor Hydro-Québec config flow and remove options

--- custom_components/hydro_quebec_consommation/config_flow.py
+++ custom_components/hydro_quebec_consommation/config_flow.py
@@ -1,36 +1,14 @@
 
 from homeassistant import config_entries
-from homeassistant.core import callback
 from .const import DOMAIN
 
 class HydroQuebecConsommationConfigFlow(config_entries.ConfigFlow):
-    """Handle a config flow for Hydro-Québec Consommation."""
+    """Config flow for Hydro-Québec Consommation."""
 
-    domain = DOMAIN  
+    domain = DOMAIN
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
-            return self.async_create_entry(
-                title="Hydro-Québec – Consommation d’énergie",
-                data={},
-            )
-
+            return self.async_create_entry(title="Hydro-Québec", data={})
         return self.async_show_form(step_id="user")
-
-    @callback
-    def async_get_options_flow(config_entry):
-        return HydroQuebecOptionsFlowHandler(config_entry)
-
-
-class HydroQuebecOptionsFlowHandler(config_entries.OptionsFlow):
-    """Handle options for Hydro-Québec integration."""
-
-    def __init__(self, config_entry):
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input=None):
-        if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
-
-        return self.async_show_form(step_id="init")
