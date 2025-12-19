cc944daaf0d690b0281ba9bf1ef73b7c977a0605
Chris <33991025+moimeme81@users.noreply.github.com>
2025-12-19 00:31:43 -0500

    Create __init__.py for hydroqc_dr component

---
+++ custom_components/hydroqc_dr/__init__.py
@@ -0,0 +1,24 @@
+
+from __future__ import annotations
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+from .const import DOMAIN
+
+PLATFORMS = ["sensor"]
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = {}
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    unloaded = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unloaded:
+        hass.data[DOMAIN].pop(entry.entry_id, None)
+    return unloaded
+
